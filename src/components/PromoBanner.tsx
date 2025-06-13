6516b6d18a3c1474f9b3348202749199d5071701
abkar <ammg.11@hotmail.com>
2025-06-13 14:28:28 +0300

    Initial commit1

---
+++ src/components/PromoBanner.tsx
@@ -0,0 +1,13 @@
+import { Box, Container, Grid } from "@mui/material";
+
+export default function PromoBanner (){
+    return <Box>
+        <Container maxWidth={"xl"}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+
+                </Grid>
+            </Grid>
+        </Container>
+    </Box>
+}
